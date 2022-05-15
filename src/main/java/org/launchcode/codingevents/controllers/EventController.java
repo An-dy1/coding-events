749b90c30b353f8efc01864daefa46ada601d5e6
Andie Johnson <ajohnson2@naic.org>
2022-05-15 09:25:35 -0500

    my exercise solutions

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,31 +8,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-  private static List<String> events = new ArrayList<>();
+//  private static List<String> events = new ArrayList<>();
+  private static Map<String, String> events = new HashMap<>();
+
 
   @GetMapping()
   public String displayAllEvents(Model model) {
+    events.put("Event 1", "Red");
+    events.put("Event 2", "Orange");
+    events.put("Event 3", "Yellow");
     model.addAttribute("events", events);
-    model.addAttribute("title", "My Coding Events");
+    model.addAttribute("title", "A different title");
+    model.addAttribute("address", "456 E West St, KC MO");
     return "events/index";
   }
 
   @GetMapping("create")
   public String renderEventForm(Model model) {
-    model.addAttribute("title", "My Coding Events");
+    model.addAttribute("title", "A different title");
     return "events/create";
   }
 
   @PostMapping("create")
   public String acceptFormSubmission(@RequestParam String eventName) {
     if(!eventName.equals("")) {
-      events.add(eventName);
+      events.put(eventName, "placeholder");
     }
     return "redirect:";
   }
