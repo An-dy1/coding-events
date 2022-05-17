435ba824286642237abe7eefa4bab8dbf6d08846
Andie Johnson <ajohnson2@naic.org>
2022-05-17 12:42:05 -0500

    add unique id in Event class

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,21 +1,41 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
 
   /* FIELDS */
   private String name;
   private String description;
+  private int eventId;
+
+  private static int nextId = 1;
 
   /* CONSTRUCTOR */
   public Event(String name, String description) {
     this.name = name;
     this.description = description;
+    this.eventId = nextId;
+    nextId++;
   }
 
   /* METHODS */
   @Override
   public String toString() {
     return name;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Event event = (Event) o;
+    return eventId == event.eventId;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(eventId);
   }
 
   /* GETTERS AND SETTERS */
@@ -33,5 +53,9 @@ public class Event {
 
   public void setDescription(String description) {
     this.description = description;
+  }
+
+  public int getEventId() {
+    return eventId;
   }
 }
