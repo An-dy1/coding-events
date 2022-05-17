b79631a3f7325ab1dd8cb0b3f11ef1f22a754540
Andie Johnson <ajohnson2@naic.org>
2022-05-17 12:20:09 -0500

    thymeleaf exercises, beginning of model unit

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,30 +18,33 @@ import java.util.Map;
 public class EventController {
 
 //  private static List<String> events = new ArrayList<>();
-  private static Map<String, String> events = new HashMap<>();
+//  private static Map<String, String> events = new HashMap<>();
+
+  private static List<Event> events = new ArrayList<>();
 
 
   @GetMapping()
   public String displayAllEvents(Model model) {
-    events.put("Event 1", "Red");
-    events.put("Event 2", "Orange");
-    events.put("Event 3", "Yellow");
+//    events.add(new Event("Event 1", "Red"));
+//    events.add(new Event("Event 2", "Orange"));
+//    events.add(new Event("Event 3", "Yellow"));
+
     model.addAttribute("events", events);
-    model.addAttribute("title", "A different title");
+    model.addAttribute("title", "Andie's dynamic coding events title");
     model.addAttribute("address", "456 E West St, KC MO");
     return "events/index";
   }
 
   @GetMapping("create")
   public String renderEventForm(Model model) {
-    model.addAttribute("title", "A different title");
+    model.addAttribute("title", "A dynamic title for the create page");
     return "events/create";
   }
 
   @PostMapping("create")
-  public String acceptFormSubmission(@RequestParam String eventName) {
-    if(!eventName.equals("")) {
-      events.put(eventName, "placeholder");
+  public String acceptFormSubmission(@RequestParam String eventName, @RequestParam String eventDescription) {
+    if(!eventName.equals("") && !eventDescription.equals("")) {
+      events.add(new Event(eventName, eventDescription));
     }
     return "redirect:";
   }
