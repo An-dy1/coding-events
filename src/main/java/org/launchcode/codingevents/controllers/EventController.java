b026fed6bab7f90e32dc3e822c14e0794049b7b5
Andie Johnson <ajohnson2@naic.org>
2022-05-23 13:33:16 -0500

    first few steps of exercises, but I realized I'm missing EventData

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,10 +3,7 @@ package org.launchcode.codingevents.controllers;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,32 +17,43 @@ public class EventController {
 //  private static List<String> events = new ArrayList<>();
 //  private static Map<String, String> events = new HashMap<>();
 
-  private static List<Event> events = new ArrayList<>();
+	private static List<Event> events = new ArrayList<>();
 
 
-  @GetMapping()
-  public String displayAllEvents(Model model) {
+	@GetMapping()
+	public String displayAllEvents(Model model) {
 //    events.add(new Event("Event 1", "Red"));
 //    events.add(new Event("Event 2", "Orange"));
 //    events.add(new Event("Event 3", "Yellow"));
 
-    model.addAttribute("events", events);
-    model.addAttribute("title", "Andie's dynamic coding events title");
-    model.addAttribute("address", "456 E West St, KC MO");
-    return "events/index";
-  }
+		model.addAttribute("events", events);
+		model.addAttribute("title", "Andie's dynamic coding events title");
+		model.addAttribute("address", "456 E West St, KC MO");
+		return "events/index";
+	}
 
-  @GetMapping("create")
-  public String renderEventForm(Model model) {
-    model.addAttribute("title", "A dynamic title for the create page");
-    return "events/create";
-  }
+	@GetMapping("create")
+	public String renderEventForm(Model model) {
+		model.addAttribute("title", "A dynamic title for the create page");
+		return "events/create";
+	}
 
-  @PostMapping("create")
-  public String acceptFormSubmission(@RequestParam String eventName, @RequestParam String eventDescription) {
-    if(!eventName.equals("") && !eventDescription.equals("")) {
-      events.add(new Event(eventName, eventDescription));
-    }
-    return "redirect:";
-  }
+	@PostMapping("create")
+	public String acceptFormSubmission(@RequestParam String eventName, @RequestParam String eventDescription) {
+		if (!eventName.equals("") && !eventDescription.equals("")) {
+			events.add(new Event(eventName, eventDescription));
+		}
+		return "redirect:";
+	}
+
+	@GetMapping("edit/${eventId}")
+	public String displayEditForm(Model model, @PathVariable int eventId) {
+
+		return "redirect:";
+	}
+
+	@PostMapping("edit")
+	public String processEditForm(int eventId, String name, String description) {
+		return "String";
+	}
 }
